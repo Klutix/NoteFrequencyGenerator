18af3ba3786e4c4a65ebe16d8cfd1eb0fee2ff11
Koki <Gabrielwohlford@gmail.com>
2018-04-23 07:54:19 -0600

    Initial commit

---
+++ noteFrequencyGenerator.cs
@@ -0,0 +1,17 @@
+class NoteFrequencyGenerator
+    {
+        //all the parts of this exprimental creation
+        static Func<int, int>    OctaveFromKeyNo = (n) => (n>3) ? n > 15 ? (n-3) / 12 + ((n-3) % 12 != 0 ? 1 : 0):1:0;
+        static Func<int, int>    GetNoReplace    = (n) => (n >= 15) ? GetNoReplace(n - 12) : (n > 12) ? GetNoReplace(n - 12):n;
+        static Func<int, double> MakeNoteFreq    = (n) => Math.Pow(2.0, ((n - 49.0) / 12.0)) * 440.0;
+        static Func<int, bool>   isSharpKey      = (n) => n == 2 || n == 5 || n == 7 || n == 10 || n == 12;
+        static Func<int, string> NoteFromKeyNo   = (n) => Convert.ToString("0AABCCDDEFFGG"[GetNoReplace(n)]);       
+        static Func<int, String> BuildFullSymbol = (n) => Convert.ToString(NoteFromKeyNo(n) + (isSharpKey(GetNoReplace(n)) ?"#":"") + OctaveFromKeyNo(n));
+       
+        public static Func<int,int,Dictionary<String, double>> GenDictForRangeOfKeys = (st, ed) => 
+         {             
+             Dictionary<String, double> dct = new Dictionary<String, double>();
+             Enumerable.Range(st, ed).ToList().ForEach(x => dct[BuildFullSymbol(x)] = MakeNoteFreq(x));
+             return dct;
+         };       
+    }
